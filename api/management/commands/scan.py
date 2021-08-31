fe209bbd53cf08005e1cdbe47fea2db1d706aaff
Jakob Malm <jakob.malm@husqvarnagroup.com>
2021-08-31 22:57:40 +0200

    Add --full-scan argument to scan CLI command

--- api/management/commands/scan.py
+++ api/management/commands/scan.py
@@ -3,10 +3,16 @@ from api.models import User
 from api.directory_watcher import scan_photos
 from api.util import logger
 import uuid
-import django_rq
 
 class Command(BaseCommand):
     help = 'scan directory for all users'
-    def handle(self, *args, **kwargs):
+
+    def add_arguments(self, parser):
+        parser.add_argument('--full-scan', action='store_true',
+            help=("Perform a full scan of all files "
+                  "(including already scanned files which haven't changed since last scan)")
+        )
+
+    def handle(self, *args, full_scan=False, **kwargs):
         for user in User.objects.all():
-            scan_photos(user,uuid.uuid4())
+            scan_photos(user, full_scan, uuid.uuid4())
